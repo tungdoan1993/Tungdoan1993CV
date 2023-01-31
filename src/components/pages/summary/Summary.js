ea82c0309b53fe5cb9b8d568265975a3fe51dd6b
tungdoan1993 <tungdoan1993@gmail.com>
2023-01-31 16:37:05 +0700

    first commit

---
+++ src/components/pages/summary/Summary.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Summary extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card brown lighten-3 z-depth-0">
+            <div className="card-content">
+                <h6 className="no-pad mt-bottom brown-text text-darken-4">
+                    <strong>ABOUT ME</strong>
+                </h6>
+                <p className="pt brown-text text-darken-4">
+                Hi, my name is Minh Tung and I am a Trainee Developer
+                </p>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
